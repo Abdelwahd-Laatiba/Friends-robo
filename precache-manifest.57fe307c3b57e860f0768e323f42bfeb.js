e8dff8992065f72601044b5a033c9df0fa09b7d3
Abdelwahd Laatiba <abdelwahdlaatiba11@gmail.com>
2020-03-27 12:02:11 +0100

    Updates

---
+++ precache-manifest.57fe307c3b57e860f0768e323f42bfeb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7ddd8dfa7beb21d7fd32724f310c73d",
+    "url": "/Friends-robo/index.html"
+  },
+  {
+    "revision": "79436e81eb103ede0f87",
+    "url": "/Friends-robo/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "84ea4b0ba245d5431638",
+    "url": "/Friends-robo/static/css/main.1565ecc7.chunk.css"
+  },
+  {
+    "revision": "79436e81eb103ede0f87",
+    "url": "/Friends-robo/static/js/2.8ca0bda2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Friends-robo/static/js/2.8ca0bda2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "84ea4b0ba245d5431638",
+    "url": "/Friends-robo/static/js/main.28f000bd.chunk.js"
+  },
+  {
+    "revision": "ccc9b3aef364e2b28b37",
+    "url": "/Friends-robo/static/js/runtime-main.9946b25e.js"
+  }
+]);
